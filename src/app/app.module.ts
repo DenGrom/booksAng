78a1b8c96f7ecd99e1c3c373a3646073ca352ad9
DenGrom <156789qq@gmail.com>
2019-03-03 09:59:05 +0200

    add compon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
+import { NavMainComponent } from './nav-main/nav-main.component';
+import { NavLeftComponent } from './nav-left/nav-left.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PaymentComponent } from './payment/payment.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMainComponent,
+    NavLeftComponent,
+    HomeComponent,
+    AboutComponent,
+    PaymentComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
