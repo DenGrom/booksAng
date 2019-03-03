e9807d875e21e333eec8ca67f47edaf60450d2a2
DenGrom <156789qq@gmail.com>
2019-03-03 10:14:47 +0200

    header c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
-import { NavMainComponent } from './nav-main/nav-main.component';
 import { NavLeftComponent } from './nav-left/nav-left.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PaymentComponent } from './payment/payment.component';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMainComponent,
     NavLeftComponent,
     HomeComponent,
     AboutComponent,
     PaymentComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
