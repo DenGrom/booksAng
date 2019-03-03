dc242661b4d961ff9fb7832afd640dcc00038ffa
DenGrom <156789qq@gmail.com>
2019-03-03 13:41:01 +0200

    transl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import {PaymentComponent} from './payment/payment.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  {path: 'payment', component: PaymentComponent}
+  { path: 'payment', component: PaymentComponent }
 ];
 
 @NgModule({
